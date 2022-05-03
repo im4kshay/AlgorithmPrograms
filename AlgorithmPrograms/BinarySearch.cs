9ae43cf48b7b5f617ba03a2314648554c17c9f09
im4kshay <akshaysayre7@gmail.com>
2022-05-04 02:41:19 +0530

    Binary Search

---
+++ AlgorithmPrograms/BinarySearch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    class BinarySearch
+    {
+        public static int BinSearch(int[] arr, int searchItem)
+        {
+            int start = 0;
+            int end = arr.Length - 1;
+            while(start <= end)
+            {
+                int mid = (start + end) / 2;
+                if(searchItem == arr[mid])
+                {
+                    return mid;
+                }
+                else if(searchItem < arr[mid])
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
--- AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/Program.cs
@@ -10,7 +10,38 @@ namespace AlgorithmPrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("=-=-=-=-=-=-=-=Data Sructures Algorithm Programs=-=-=-=-=-=-=-=");
+            string flag = "Y";
+            while (flag == "Y" || flag == "y")
+            {
+                Console.WriteLine("=-=-=-=-=-=-=-=Data Sructures Algorithm Programs=-=-=-=-=-=-=-=");
+
+                Console.WriteLine("1.Binary Search\n");
+                Console.WriteLine("Enter Your Option");
+                int num = Convert.ToInt32(Console.ReadLine());
+                switch (num)
+                {
+                    case 1:
+                        Console.WriteLine("Please provide input search item:");
+                        int searchItem= int.Parse(Console.ReadLine());
+                        int[] arr = { 5, 11, 20, 25, 33, 49, 50, 62, 67, 73 };
+                        int result = BinarySearch.BinSearch(arr, searchItem);
+                        if(result<0)
+                        {
+                            Console.WriteLine($"{searchItem} is not fouund");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{searchItem} is found at index:{result}");
+                        }
+                        break; 
+
+                    default:
+                        Console.WriteLine("-------Enter the valid option---");
+                        break;
+                }
+                Console.WriteLine("\nDo you want to continue");
+                flag = Console.ReadLine();
+            }
             Console.ReadLine();
         }
     }
